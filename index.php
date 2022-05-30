7d7478956d649b229a0c3f044423d43874451125
Samuelsntr <dodisamuel20@gmail.com>
2022-05-30 09:52:54 +0800

    showing img

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ while($post = $posts->fetch_object()) {
    <div class="card">
        <a href="post.php?id=<?php echo $post->id ?>"><h2><?php echo $post->title; ?></h2></a>
        <h5><?php echo $post->createdAt; ?></h5>
-       <div><?php echo "<img src='uploads/$image[image]' width='70' height='90' />";?></div>
-                <div><?php echo $image['tmp_name']?></div>
+       <div><img src='uploads/<?php echo $post->image ?>'></div>
    </div>
    <?php
 }
